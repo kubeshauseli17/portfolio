d3b6789fb2000016183ab19173c98d3308881548
Eijah Kubes <kubeshauseli17@gmail.com>
2022-10-21 16:23:12 -0700

    init commit

---
+++ components/navBar.js
@@ -0,0 +1,12 @@
+// import builtin link from next
+import Link from "next/link"
+
+export default function NavBar() {
+    return(
+        <nav>
+            <Link href='/'>Home</Link>
+
+            <Link href='/about'>About Me</Link>
+        </nav>
+    )
+}
---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import styles from '../styles/About.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+
+export default function About() {
+    return(
+        <div>
+            <h1>About Me</h1>
+            <p>Hi  am Elijah, I'm a software engineer now</p>
+
+
+            {/* load an mage locally */}
+            <Image
+                src='/vercel.svg'
+                alt='logo'
+                width={300}
+                height={300}
+            />
+
+            {/* load an image hosted on anothe domain */}
+            <Image
+                src='https://placekitten.com/400/500'
+                alt='portflio cat'
+                width={400}
+                height={500}
+            />
+            
+        </div>
+    )
+};
